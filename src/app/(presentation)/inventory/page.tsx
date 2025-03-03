71f50d87abbd5532bb4a7736ca0fc313885d86f1
awais512 <raza.awais25@gmail.com>
2025-03-03 19:51:09 +0000

    Pagination done

--- src/app/(presentation)/inventory/page.tsx
+++ src/app/(presentation)/inventory/page.tsx
@@ -3,30 +3,70 @@ import { prisma } from "@/lib/prisma";
 import { ClassifiedsList } from "./_components/classified-list";
 import { redis } from "@/lib/redis-store";
 import { getSourceId } from "@/lib/source-id";
+import { CustomPagination } from "@/components/shared/custom-pagination";
+import { routes } from "@/config/routes";
+import { z } from "zod";
+import { CLASSIFIED_PER_PAGE } from "@/config/constants";
+
+const PageSchema = z
+  .string()
+  .transform((val) => Math.max(Number(val), 1))
+  .optional();
 
 const getInventory = async (searchParams: AwaitedPageProps["searchParams"]) => {
+  const validPage = PageSchema.parse(searchParams?.page);
+
+  const page = validPage ? validPage : 1;
+  const offset = (page - 1) * CLASSIFIED_PER_PAGE;
+
   return prisma.classified.findMany({
+    where: {},
     include: {
-      images: true,
+      images: { take: 1 },
     },
+    skip: offset,
+    take: CLASSIFIED_PER_PAGE,
   });
 };
 
 const InventoryPage = async (props: PageProps) => {
   const searchParams = await props.searchParams;
   const classifieds = await getInventory(searchParams);
-  const count = await prisma.classified.count();
+  const count = await prisma.classified.count({ where: {} });
 
   const sourceId = await getSourceId();
 
   const favourites = await redis.get<Favourites>(sourceId ?? "");
+  const totalPages = Math.ceil(count / CLASSIFIED_PER_PAGE);
 
   return (
-    <div className="p-4 mt-4">
-      <ClassifiedsList
-        classifieds={classifieds}
-        favourites={favourites ? favourites.ids : []}
-      />
+    <div className="flex">
+      {/* <Sidebar /> */}
+      <div className="flex-1 p-4 bg-white">
+        <div className="flex space-y-2 flex-col items-center justify-center pb-4 -mt-1">
+          <div className="flex justify-between items-center w-full">
+            <h2 className="text-sm md:text-base lg:text-xl font-semibold min-w-fit">
+              We have found {count} classifieds
+            </h2>
+            {/* Filter */}
+          </div>
+          <CustomPagination
+            baseUrl={routes.inventory}
+            totalPages={totalPages}
+            styles={{
+              paginationRoot: "flex justify-end",
+              paginationPrevious: "",
+              paginationNext: "",
+              paginationLink: "border-none active:border text-black",
+              paginationLinkActive: "",
+            }}
+          />
+          <ClassifiedsList
+            classifieds={classifieds}
+            favourites={favourites ? favourites.ids : []}
+          />
+        </div>
+      </div>
     </div>
   );
 };
--- src/config/constants.ts
+++ src/config/constants.ts
@@ -2,3 +2,5 @@ export const imageSources = {
   classifiedPlaceholder:
     "https://res.cloudinary.com/awais512/image/upload/v1740850790/classified-placeholder.jpg",
 };
+
+export const CLASSIFIED_PER_PAGE = 3;
