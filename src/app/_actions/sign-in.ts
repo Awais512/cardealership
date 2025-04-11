50f57f14f120854c61ddcc8365ce32a3bb5f3a19
awais512 <raza.awais25@gmail.com>
2025-04-11 18:39:38 +0100

    Adding Ratelimiter

--- src/app/_actions/sign-in.ts
+++ src/app/_actions/sign-in.ts
@@ -5,9 +5,13 @@ import { SignInSchema } from "../schemas/auth.schema";
 import { routes } from "@/config/routes";
 import { PrevState } from "@/config/types";
 import { isRedirectError } from "next/dist/client/components/redirect-error";
+import { genericRateLimit } from "@/lib/rate-limiter";
 
 export const signInAction = async (_: PrevState, formData: FormData) => {
   try {
+    const limiterError = await genericRateLimit("login");
+    if (limiterError) return limiterError;
+
     const email = formData.get("email") as string;
     const password = formData.get("password") as string;
 
